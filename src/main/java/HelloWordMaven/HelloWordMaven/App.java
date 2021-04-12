726e3dc3566413835d6a88cb6cf8594f42fe2061
Divyabd <divyagowda8393@gmail.com>
2021-04-12 17:48:57 +0530

    initial commit

---
+++ src/main/java/HelloWordMaven/HelloWordMaven/App.java
@@ -0,0 +1,14 @@
+package HelloWordMaven.HelloWordMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Divya" );
+        System.out.println( "Welcome to possible" );
+    }
+}
